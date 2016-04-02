ffa7644a8dea78e6da1758c1efb9d927314605bc
Anton Rogov <anton@blackbits.pro>
2016-04-02 13:23:48 +0300

    handle node.hosts

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,3 +5,14 @@ include_recipe 'core-bits::swap'
 include_recipe 'core-bits::ntpd'
 include_recipe 'core-bits::env'
 include_recipe 'core-bits::owner'
+
+if node[:hosts]
+  node.hosts.each do |ip, names|
+    hostsfile_entry ip do
+      ip_address ip
+      hostname names[0]
+      aliases names[1..-1]
+      action :append
+    end
+  end
+end
